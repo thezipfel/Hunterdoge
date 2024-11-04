258f131cb288d9328e6e32a88da1d7cc8254710d
thezipfel <119841820+thezipfel@users.noreply.github.com>
2024-11-04 09:56:40 +0100

    Add files via upload

---
+++ HunterDogeContent-main/src/App.js
@@ -0,0 +1,182 @@
+import React, {Component} from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="App">
+                            <div className="card">
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <img
+                                        src="content/Logo.png"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        Logo for our HunterDoge
+                                    </p>
+                                    <a href="content/Logo.png" className="btn btn-primary">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="App">
+                            <div className="card">
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <img
+                                        src="content/Emojis/Amazed.png"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        Amazed HunterDoge
+                                    </p>
+                                    <a href="content/Emojis/Amazed.png" className="btn btn-primary">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="App">
+                            <div className="card">
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <img
+                                        src="content/Emojis/Angel.png"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        Angel HunterDoge
+                                    </p>
+                                    <a href="content/Emojis/Angel.png" className="btn btn-primary">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="App">
+                            <div className="card">
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <img
+                                        src="content/Emojis/Big_eyes.png"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        Big Eyes HunterDoge
+                                    </p>
+                                    <a href="content/Emojis/Big_eyes.png" className="btn btn-primary">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="App">
+                            <div className="card">
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <img
+                                        src="content/Emojis/Confident.png"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        Confident HunterDoge
+                                    </p>
+                                    <a href="content/Emojis/Confident.png" className="btn btn-primary">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="App">
+                            <div className="card">
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <img
+                                        src="content/Emojis/Coolio.png"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        Cool HunterDoge
+                                    </p>
+                                    <a href="content/Emojis/Coolio.png" className="btn btn-primary">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="App">
+                            <div className="card">
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <img
+                                        src="content/Emojis/Disappointed.png"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        Dissapointed HunterDoge
+                                    </p>
+                                    <a href="content/Emojis/Big_eyes.png" className="btn btn-primary">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="App">
+                            <div className="card">
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <img
+                                        src="content/Emojis/Confident.png"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        Confident HunterDoge
+                                    </p>
+                                    <a href="content/Emojis/Confident.png" className="btn btn-primary">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="App">
+                            <div className="card">
+                                <div className="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                    <img
+                                        src="content/Emojis/Coolio.png"
+                                        className="img-fluid"
+                                    />
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        Cool HunterDoge
+                                    </p>
+                                    <a href="content/Emojis/Coolio.png" className="btn btn-primary">Download</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
